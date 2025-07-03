7c88540bb6807ec5601401e856b4a4e29c3c6132
three-co1ors <131871273+three-co1ors@users.noreply.github.com>
2025-07-03 15:04:26 +0900

    Revert "feat: 맵 페이드 & 플래시 설정"

--- Assets/Script/TransferMap.cs
+++ Assets/Script/TransferMap.cs
@@ -1,46 +1,32 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class TransferMap : MonoBehaviour
 {
-    public string transferMapName; // 이동할 맵의 이름
+    public string transferMapName;
+
+    private PlayerManager thePlayer;
+    private CameraManager theCamera;
+    public PolygonCollider2D targetBound;
 
     public Transform target;
-    public PolygonCollider2D targetBound;  // 변경
-
-    private CameraManager theCamera;
-    [SerializeField] private PlayerManager thePlayer;
-    private FadeManager theFade;
-    private OrderManager theOrder;
-
+    // Start is called before the first frame update
     void Start()
     {
-        thePlayer = FindObjectOfType<PlayerManager>();
-        theCamera = FindObjectOfType<CameraManager>();
-        theFade = FindObjectOfType<FadeManager>();
-        theOrder = FindObjectOfType<OrderManager>();
+        theCamera = FindFirstObjectByType<CameraManager>();
+        thePlayer = FindFirstObjectByType<PlayerManager>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.gameObject.name == "player")
         {
-            StartCoroutine(TransferCoroutine());
+            theCamera.SetBound(targetBound);
+            thePlayer.currentMapName = transferMapName;
+            theCamera.transform.position = new Vector3(target.transform.position.x, target.transform.position.y, theCamera.transform.position.z);
+            thePlayer.transform.position = target.transform.position;
         }
-    }
-
-    IEnumerator TransferCoroutine()
-    {
-       theOrder.NotMove();
-        theFade.FadeOut();
-        yield return new WaitForSeconds(1f);
-        thePlayer.currentMapName = transferMapName;
-        theCamera.SetBound(targetBound); 
-        theCamera.transform.position = new Vector3(target.position.x, target.position.y, theCamera.transform.position.z);
-        thePlayer.transform.position = target.position;
-        theFade.FadeIn();
-        yield return new WaitForSeconds(0.5f);
-        theOrder.Move();
     }
 }
