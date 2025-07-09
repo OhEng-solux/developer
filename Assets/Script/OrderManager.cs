692ddcba06e0c23536456803355c0f333fdcd867
YUSUHYEON <ysh83@sookmyung.ac.kr>
2025-07-10 04:45:04 +0900

    fix: dialogue animation bug

--- Assets/Script/OrderManager.cs
+++ Assets/Script/OrderManager.cs
@@ -4,11 +4,10 @@ using System.Collections;
 
 public class OrderManager : MonoBehaviour
 {
-    private PlayerManager thePlayer; // �̺�Ʈ ���߿� Ű �Է� ó�� ����
+    private PlayerManager thePlayer; // 이벤트 도중에 키 입력 처리 방지
+    // private MovingObject[] characters; -> 배열은 고정된 크기를 가지므로 NPC의 개수가 변할 때마다 크기를 조정해야 함
+    // -> 배열 대신 list 사용
     private List<MovingObject> characters;
-    // MovingObject[] -> �迭�� ũ�� �����Ǹ� ���� �Ұ��� ������ ����� ������, List ���
-    // Add(), Remove(), Clear()
-
     void Start()
     {
         thePlayer = FindAnyObjectByType<PlayerManager>();
@@ -24,13 +23,12 @@ public class OrderManager : MonoBehaviour
     public List<MovingObject> ToList()
     {
         List<MovingObject> tempList = new List<MovingObject>();
-        MovingObject[] temp = FindObjectsOfType<MovingObject>(); // MovingObject�� �޸� ��� ��ü�� ã�Ƽ� ��ȯ�� �� (objects)
+        MovingObject[] temp = FindObjectsByType<MovingObject>(FindObjectsSortMode.None); // 씬에 있는 모든 MovingObject를 찾아서 temp 배열에 저장
 
         for (int i = 0; i < temp.Length; i++)
         {
-            tempList.Add(temp[i]);
+            tempList.Add(temp[i]); // temp 배열의 요소를 리스트에 추가
         }
-
         return tempList;
     }
 
@@ -44,68 +42,57 @@ public class OrderManager : MonoBehaviour
         thePlayer.notMove = false;
     }
 
-    // �÷��̾ �Ѱ� ����� ������ NPC�� �÷��̾��� ��ġ�� ���ؼ� ���ǹ� ����
-    public void SetThorought(string _name) // �� �ձ�
+    public void SetThorought(string _name)
     {
         for (int i = 0; i < characters.Count; i++)
-        { // ����Ʈ�� ũ��� Count
+        {
+            if( _name == characters[i].characterName) // 이름이 일치하는 오브젝트 찾기
             {
-                if (_name == characters[i].characterName)
-                {
-                    characters[i].boxCollider.enabled = false;
-                }
-            }
-        }
-    }
-    public void SetUnThorought(string _name) // �ٽ� ��� ���ϰ�
-    {
-        for (int i = 0; i < characters.Count; i++)
-        { // ����Ʈ�� ũ��� Count
-            {
-                if (_name == characters[i].characterName)
-                {
-                    characters[i].boxCollider.enabled = true;
-                }
+                characters[i].boxCollider.enabled = false; // BoxCollider2D 비활성화
             }
         }
     }
 
-    public void SetTransparent(string _name) // ������ ���� (�������)
+     public void SetUnThorought(string _name)
     {
         for (int i = 0; i < characters.Count; i++)
-        { // ����Ʈ�� ũ��� Count
+        {
+            if( _name == characters[i].characterName) // 이름이 일치하는 오브젝트 찾기
             {
-                if (_name == characters[i].characterName)
-                {
-                    characters[i].gameObject.SetActive(false);
-                }
+                characters[i].boxCollider.enabled = true; // BoxCollider2D 활성화
             }
         }
     }
 
-    public void SetUnTransparent(string _name) // ������ ���� (�ٽ� ����)
+    public void SetTransparent(string _name)
     {
         for (int i = 0; i < characters.Count; i++)
-        { // ����Ʈ�� ũ��� Count
+        {
+            if( _name == characters[i].characterName) // 이름이 일치하는 오브젝트 찾기
             {
-                if (_name == characters[i].characterName)
-                {
-                    characters[i].gameObject.SetActive(true);
-                }
+                characters[i].gameObject.SetActive(false);
             }
         }
     }
 
-    public void Move(string _name, string _dir)
+    public void SetUnTransparent(string _name)
     {
         for (int i = 0; i < characters.Count; i++)
-        { // ����Ʈ�� ũ��� Count
+        {
+            if( _name == characters[i].characterName) // 이름이 일치하는 오브젝트 찾기
             {
-                if (_name == characters[i].characterName)
-                {
-                    // ���� �Լ� ����
-                    characters[i].Move(_dir);
-                }
+                characters[i].gameObject.SetActive(true);
+            }
+        }
+    }
+
+     public void Move(string _name, string _dir)
+    {
+        for (int i = 0; i < characters.Count; i++)
+        {
+            if( _name == characters[i].characterName) // 이름이 일치하는 오브젝트 찾기
+            {
+                characters[i].Move(_dir);
             }
         }
     }
@@ -113,28 +100,25 @@ public class OrderManager : MonoBehaviour
     public void Turn(string _name, string _dir)
     {
         for (int i = 0; i < characters.Count; i++)
-        { // ����Ʈ�� ũ��� Count
+        {
+            if( _name == characters[i].characterName) // 이름이 일치하는 오브젝트 찾기
             {
-                if (_name == characters[i].characterName)
+                characters[i].animator.SetFloat("DirX", 0f);
+                characters[i].animator.SetFloat("DirY", 0f);
+                switch(_dir)
                 {
-                    // �ʱ�ȭ
-                    characters[i].animator.SetFloat("DirX", 0f);
-                    characters[i].animator.SetFloat("DirY", 0f);
-                    switch (_dir)
-                    {
-                        case "UP":
-                            characters[i].animator.SetFloat("DirY", 1f);
-                            break;
-                        case "DOWN":
-                            characters[i].animator.SetFloat("DirY", -1f);
-                            break;
-                        case "LEFT":
-                            characters[i].animator.SetFloat("DirX", -1f);
-                            break;
-                        case "RIGHT":
-                            characters[i].animator.SetFloat("DirX", 1f);
-                            break;
-                    }
+                    case "UP":
+                        characters[i].animator.SetFloat("DirY", 1f);
+                        break;
+                    case "DOWN":
+                        characters[i].animator.SetFloat("DirY", -1f);
+                        break;
+                    case "LEFT":
+                        characters[i].animator.SetFloat("DirX", -1f);
+                        break;
+                    case "RIGHT":
+                        characters[i].animator.SetFloat("DirX", 1f);
+                        break;
                 }
             }
         }
