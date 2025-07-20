b819c272c41abd8dd35177783388e1366babdb08
leece03 <164038699+leece03@users.noreply.github.com>
2025-07-21 02:24:12 +0900

    feat: bound 스크립트 수정
    bound name 저장 가능

--- Assets/Script/Bound.cs
+++ Assets/Script/Bound.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class bound : MonoBehaviour
 {
     private PolygonCollider2D Bound;
-
+    public string boundName;//바운드 이름 불러오기
     private CameraManager theCamera;
 
     // Start is called before the first frame update
@@ -15,7 +15,12 @@ public class bound : MonoBehaviour
         var theCamera = FindFirstObjectByType<CameraManager>();
         theCamera.SetBound(Bound);
     }
-
+    public void setBound()
+    {
+        if (theCamera!=null) {
+            theCamera.SetBound(Bound); 
+        }
+    }
     // Update is called once per frame
     void Update()
     {
