4aadb61ce615a0565b483c9bc97ec37165a251d9
sssuhha <ysh83@sookmyung.ac.kr>
2025-07-29 02:31:43 +0900

    Revert "feat: taeju_demon 등장 로직 수정"
    This reverts commit d1aaa7c73a2ca65f80ba4eea2bf93363101ab255.

--- Assets/Script/TaejuChase.cs
+++ Assets/Script/TaejuChase.cs
@@ -97,11 +97,6 @@ public class TaejuChase : MonoBehaviour
     public void StartChase()
     {
         isChasing = true;
-        var spriteRenderer = GetComponent<SpriteRenderer>();
-        if (spriteRenderer != null)
-            spriteRenderer.enabled = true;
-
-        animator.SetBool("Walking", true);
     }
 
     public void StopChase()
