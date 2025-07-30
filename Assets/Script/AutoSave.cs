d92d2d198b8289f022b3acfe624e12e2207d427e
leece03 <164038699+leece03@users.noreply.github.com>
2025-07-31 02:03:39 +0900

    feat: AutoSave 스크립트 생성

---
+++ Assets/Script/AutoSave.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class AutoSave : MonoBehaviour
+{
+    public static AutoSave instance;
+    private bool flag = true;
+    public bool autoSave=false;
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (flag == true)
+        {
+            autoSave = true;
+        }
+        flag = false;
+        //Debug.Log("충돌");
+    }
+    public bool IsAutoSave()
+    {
+        return autoSave;
+    }
+
+}
