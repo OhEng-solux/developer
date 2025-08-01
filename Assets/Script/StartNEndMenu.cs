b9ab88a010bcc5632266b2c555e9e760baf47820
three-co1ors <131871273+three-co1ors@users.noreply.github.com>
2025-08-01 14:31:01 +0900

    [FEAT] Start 씬 뒤로가기 버튼 추가
    - 뒤로가기 버튼 추가
    - 종료 시 종료하시겠습니까? 팝업

--- Assets/Script/StartNEndMenu.cs
+++ Assets/Script/StartNEndMenu.cs
@@ -19,13 +19,14 @@ public class StartNEndMenu : MonoBehaviour
     private Color selectedColor = Color.white;
     private Color unselectedColor = Color.gray;
 
+    private bool isQuitPopupActive = false; // 종료 팝업 활성 상태 플래그
+
     private void Awake()
     {
         if (instance == null)
             instance = this;
         else
             Destroy(gameObject);
-
     }
 
     void Start()
@@ -37,11 +38,44 @@ public class StartNEndMenu : MonoBehaviour
     void Update()
     {
         Debug.Log("현재 씬 이름: " + gameObject.scene.name);
-        if (!IsPanelActive() && (SaveManager.instance == null || SaveManager.instance.IsSaveActive())) return;
-        if (gameObject.scene.name=="Ending_Bad"&&(DialogueManager.instance == null || DialogueManager.instance.talking))
+
+        // 종료 팝업이 활성 상태면 ESC로 팝업 닫기, 그 외 입력 무시
+        if (isQuitPopupActive)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                PopupManager.instance.ClosePopup();
+                isQuitPopupActive = false;
+
+                Panel.SetActive(true);
+                selectedIndex = 0;
+                HighlightButton();
+
+                Debug.Log("종료 팝업 ESC로 닫힘 - 메뉴 복귀");
+            }
+            return;
+        }
+
+        // SavePanel이 켜져있으면 ESC 눌러서 세이브 창 닫고 메뉴판넬 보여주기
+        if (SaveManager.instance != null && SaveManager.instance.IsSaveActive())
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                SaveManager.instance.CloseSave();
+                Panel.SetActive(true);
+                selectedIndex = 0;
+                HighlightButton();
+                return;
+            }
+            // 세이브 창 열렸을 때는 메뉴 이동이나 선택 무시
+            return;
+        }
+
+        // End 씬 대화 중일 경우 메뉴 비활성화
+        if (gameObject.scene.name == "Ending_Bad" && (DialogueManager.instance == null || DialogueManager.instance.talking))
         {
             Panel.gameObject.SetActive(false);
-            return; // 대화 중이면 더 이상 진행 X
+            return;
         }
 
         if (Input.GetKeyDown(KeyCode.UpArrow))
@@ -61,6 +95,16 @@ public class StartNEndMenu : MonoBehaviour
             theAudio.Play(enterSound);
             OnSelect(selectedIndex);
         }
+        else if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameObject.scene.name != "Start")
+            {
+                Debug.Log("ESC 눌림 - 메뉴로 복귀");
+                Panel.SetActive(true);
+                selectedIndex = 0;
+                HighlightButton();
+            }
+        }
     }
 
     void HighlightButton()
@@ -68,12 +112,10 @@ public class StartNEndMenu : MonoBehaviour
         for (int i = 0; i < buttons.Length; i++)
         {
             ColorBlock cb = buttons[i].colors;
-
             cb.normalColor = (i == selectedIndex) ? selectedColor : unselectedColor;
             cb.highlightedColor = cb.normalColor;
             cb.pressedColor = cb.normalColor;
             cb.selectedColor = cb.normalColor;
-
             buttons[i].colors = cb;
         }
     }
@@ -81,49 +123,79 @@ public class StartNEndMenu : MonoBehaviour
     void OnSelect(int idx)
     {
         if (gameObject.scene.name == "Start")
-        {           
+        {
             switch (idx)
             {
                 case 0: // 처음부터 시작
                     Debug.Log("처음부터 시작");
                     SceneManager.LoadScene("Prologue");
                     break;
-                case 1: // SavePanel 팝업
+
+                case 1: // 세이브 패널 열기
                     Debug.Log("세이브 패널 팝업");
                     SaveManager.instance.StartCoroutine(SaveManager.instance.OpenSave());
                     Panel.gameObject.SetActive(false);
                     break;
+
                 case 2: // 종료
-                    Debug.Log("게임 종료");
-                    Application.Quit();
+                    Debug.Log("게임 종료 선택됨");
+
+                    isQuitPopupActive = true; // 종료 팝업 활성 상태로 변경
+
+                    PopupManager.instance.ShowChoicePopup(
+                        "게임을 종료하시겠습니까?",
+                        () => {
+                            Application.Quit();
 #if UNITY_EDITOR
-                UnityEditor.EditorApplication.isPlaying = false;
+                            UnityEditor.EditorApplication.isPlaying = false;
 #endif
+                        },
+                        () => {
+                            // '아니오' 선택 시 팝업 닫고 메뉴 복귀
+                            PopupManager.instance.ClosePopup();
+
+                            isQuitPopupActive = false;
+
+                            Panel.SetActive(true);
+                            selectedIndex = 0;
+                            HighlightButton();
+
+                            Debug.Log("종료 취소 > 메뉴 복귀");
+                        }
+                    );
                     break;
+
             }
         }
         else
         {
-            //Panel.gameObject.SetActive(false);
+            // Start 씬이 아닐 때 메뉴 동작
             switch (idx)
             {
-                case 0: // 다시하기
+                case 0: // 다시하기 (세이브 패널 열기)
                     Debug.Log("다시 시작");
                     SaveManager.instance.StartCoroutine(SaveManager.instance.OpenSave());
                     Panel.gameObject.SetActive(false);
                     break;
-                case 1: // 
+
+                case 1: // 메인으로 돌아가기
                     Debug.Log("메인으로");
                     SceneManager.LoadScene("Start");
                     break;
             }
         }
-        return;
     }
-    public bool IsPanelActive() //팝업이 떠있는지 외부에서 확인할 수 있도록 함
+
+    public bool IsPanelActive()
     {
-        Debug.Log("IsPanelActive 팝업중"+ Panel.activeSelf);
+        Debug.Log("IsPanelActive: " + Panel.activeSelf);
         return Panel.activeSelf;
     }
 
+    public void ShowPanel()
+    {
+        Panel.SetActive(true);
+        selectedIndex = 0;
+        HighlightButton();
+    }
 }
