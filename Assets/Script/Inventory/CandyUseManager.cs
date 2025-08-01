4d8e8e1354fdef02b34b6977f53c09595d74660a
eisyxx <167505848+eisyxx@users.noreply.github.com>
2025-08-01 23:29:51 +0900

    feat: Chase Item Effect

--- Assets/Script/Inventory/CandyUseManager.cs
+++ Assets/Script/Inventory/CandyUseManager.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class CandyUseManager : MonoBehaviour
 {
     [SerializeField] private float stunDuration = 3f;
+    [SerializeField] private GameObject freezeEffectObject;
 
     public void ActivateEffect()
     {
@@ -18,11 +19,19 @@ public class CandyUseManager : MonoBehaviour
             chaser.StopChase();
             Debug.Log("[CandyUse] 추격자 정지 시작");
 
+            // 이펙트 켜기
+            if (freezeEffectObject != null)
+                freezeEffectObject.SetActive(true);
+
             yield return new WaitForSeconds(stunDuration);
 
             chaser.StartChase();
             chaser.PauseChase(false); // Candy로 멈춘 후, 일시정지 상태는 강제로 풀어줌
             Debug.Log("[CandyUse] 추격자 정지 해제");
+
+            // 이펙트 끄기
+            if (freezeEffectObject != null)
+                freezeEffectObject.SetActive(false);
         }
         else
         {
